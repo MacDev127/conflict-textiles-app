e78a92216399c38b6ac60581a6af15dfd4d4d05c
MacDev <your_email@example.com>
2024-04-09 12:07:16 +0100

    addressed textile-details page issue, added responsive styled

--- resources/js/Pages/TextileDetails/TextileDetail.jsx
+++ resources/js/Pages/TextileDetails/TextileDetail.jsx
@@ -9,140 +9,144 @@ const TextileDetail = ({ textileDetail }) => {
     return (
         <>
             <Navbar />
-            <div className="td__container">
-                <section className="td__image">
-                    <div className="td__image-container">
-                        <div className="modalImage">
-                            <TransformWrapper
-                                defaultScale={1}
-                                defaultPositionX={100}
-                                defaultPositiony={200}
+            <section className="textile__details">
+                <div className="td__container">
+                    <div className="modalImage">
+                        <TransformWrapper
+                            defaultScale={1}
+                            defaultPositionX={100}
+                            defaultPositiony={200}
+                        >
+                            <TransformComponent>
+                                <img
+                                    src={textileDetail.image}
+                                    alt="Enlarged View"
+                                />
+                            </TransformComponent>
+                        </TransformWrapper>
+                    </div>
+                    <div className="td__item">
+                        <div className="td__item-title-wrapper">
+                            <div
+                                key={textileDetail.id}
+                                className="td__item-title"
                             >
-                                <TransformComponent>
-                                    <img
-                                        src={textileDetail.image}
-                                        alt="Enlarged View"
-                                    />
-                                </TransformComponent>
-                            </TransformWrapper>
+                                <h1>{textileDetail.title}</h1>
+                                <h4>Amazing Subtitle</h4>
+                            </div>
+                        </div>
+
+                        <div className="td__item-intro">
+                            <p>
+                                Lorem ipsum dolor sit, amet consectetur
+                                adipisicing elit. Est quis libero placeat
+                                voluptas unde, cum et animi numquam similique?
+                                Iure incidunt, ipsum laudantium voluptate odio,
+                                consectetur, perferendis quo temporibus corrupti
+                                quis vero. Ullam, illo? Veritatis nulla ipsum
+                                minima ducimus dolor.
+                            </p>
+                        </div>
+                        <div className="td__item-info">
+                            <ul>
+                                <div key={textileDetail.id}>
+                                    <li>
+                                        <label className="td__item-info-title">
+                                            Maker:
+                                        </label>
+                                        <span className="td__item-info-value">
+                                            {textileDetail.maker}
+                                        </span>
+                                    </li>
+                                    <li>
+                                        <label className="td__item-info-title">
+                                            Country of Origin:
+                                        </label>
+                                        <span className="td__item-info-value">
+                                            {textileDetail.country_of_origin}{" "}
+                                        </span>
+                                    </li>
+                                    <li>
+                                        <label className="td__item-info-title">
+                                            Year Produced:
+                                        </label>
+                                        <span className="td__item-info-value">
+                                            {textileDetail.year_produced}
+                                        </span>
+                                    </li>
+                                    <li>
+                                        <label className="td__item-info-title">
+                                            Size:
+                                        </label>
+                                        <span className="td__item-info-value">
+                                            {textileDetail.size}
+                                        </span>
+                                    </li>
+                                    <li>
+                                        <label className="td__item-info-title">
+                                            Materials:
+                                        </label>
+                                        <span className="td__item-info-value">
+                                            {textileDetail.materials}
+                                        </span>
+                                    </li>
+                                    <li>
+                                        <label className="td__item-info-title">
+                                            Owner:
+                                        </label>
+                                        <span className="td__item-info-value">
+                                            {textileDetail.owner}
+                                        </span>
+                                    </li>
+                                    <li>
+                                        <label className="td__item-info-title">
+                                            Provenance:
+                                        </label>
+                                        <span className="td__item-info-value">
+                                            {textileDetail.provenance}
+                                        </span>
+                                    </li>
+                                    <li>
+                                        <label className="td__item-info-title">
+                                            Authenticity:
+                                        </label>
+                                        <span className="td__item-info-value">
+                                            {textileDetail.authenticity}
+                                        </span>
+                                    </li>
+                                    <li>
+                                        <label className="td__item-info-title">
+                                            Photographer:
+                                        </label>
+                                        <span className="td__item-info-value">
+                                            {textileDetail.photographer}
+                                        </span>
+                                    </li>
+                                    <li>
+                                        <label className="td__item-info-title">
+                                            Type:
+                                        </label>
+                                        <span className="td__item-info-value">
+                                            {textileDetail.type}
+                                        </span>
+                                    </li>
+                                    <li>
+                                        <label className="td__item-info-title">
+                                            Location:
+                                        </label>
+                                        <span className="td__item-info-value">
+                                            {textileDetail.location}
+                                        </span>
+                                    </li>
+                                </div>
+                            </ul>
+                        </div>
+                        <div className="td__item-card">
+                            <Accordian textileDetail={textileDetail} />
                         </div>
                     </div>
-                </section>
-                <section className="td__item">
-                    <div key={textileDetail.id} className="td__item-title">
-                        <h1>{textileDetail.title}</h1>
-                        <h4>Amazing Subtitle</h4>
-                    </div>
-
-                    <div className="td__item-intro">
-                        <p>
-                            Lorem ipsum dolor sit, amet consectetur adipisicing
-                            elit. Est quis libero placeat voluptas unde, cum et
-                            animi numquam similique? Iure incidunt, ipsum
-                            laudantium voluptate odio, consectetur, perferendis
-                            quo temporibus corrupti quis vero. Ullam, illo?
-                            Veritatis nulla ipsum minima ducimus dolor.
-                        </p>
-                    </div>
-                    <div className="td__item-info">
-                        <ul>
-                            <div key={textileDetail.id}>
-                                <li>
-                                    <label className="td__item-info-title">
-                                        Maker:
-                                    </label>
-                                    <span className="td__item-info-value">
-                                        {textileDetail.maker}
-                                    </span>
-                                </li>
-                                <li>
-                                    <label className="td__item-info-title">
-                                        Country of Origin:
-                                    </label>
-                                    <span className="td__item-info-value">
-                                        {textileDetail.country_of_origin}{" "}
-                                    </span>
-                                </li>
-                                <li>
-                                    <label className="td__item-info-title">
-                                        Year Produced:
-                                    </label>
-                                    <span className="td__item-info-value">
-                                        {textileDetail.year_produced}
-                                    </span>
-                                </li>
-                                <li>
-                                    <label className="td__item-info-title">
-                                        Size:
-                                    </label>
-                                    <span className="td__item-info-value">
-                                        {textileDetail.size}
-                                    </span>
-                                </li>
-                                <li>
-                                    <label className="td__item-info-title">
-                                        Materials:
-                                    </label>
-                                    <span className="td__item-info-value">
-                                        {textileDetail.materials}
-                                    </span>
-                                </li>
-                                <li>
-                                    <label className="td__item-info-title">
-                                        Owner:
-                                    </label>
-                                    <span className="td__item-info-value">
-                                        {textileDetail.owner}
-                                    </span>
-                                </li>
-                                <li>
-                                    <label className="td__item-info-title">
-                                        Provenance:
-                                    </label>
-                                    <span className="td__item-info-value">
-                                        {textileDetail.provenance}
-                                    </span>
-                                </li>
-                                <li>
-                                    <label className="td__item-info-title">
-                                        Authenticity:
-                                    </label>
-                                    <span className="td__item-info-value">
-                                        {textileDetail.authenticity}
-                                    </span>
-                                </li>
-                                <li>
-                                    <label className="td__item-info-title">
-                                        Photographer:
-                                    </label>
-                                    <span className="td__item-info-value">
-                                        {textileDetail.photographer}
-                                    </span>
-                                </li>
-                                <li>
-                                    <label className="td__item-info-title">
-                                        Type:
-                                    </label>
-                                    <span className="td__item-info-value">
-                                        {textileDetail.type}
-                                    </span>
-                                </li>
-                                <li>
-                                    <label className="td__item-info-title">
-                                        Location:
-                                    </label>
-                                    <span className="td__item-info-value">
-                                        {textileDetail.location}
-                                    </span>
-                                </li>
-                            </div>
-                        </ul>
-                    </div>
-                    <div className="td__item-card">
-                        <Accordian textileDetail={textileDetail} />
-                    </div>
-                </section>
-            </div>
+                </div>
+            </section>
 
             <Footer />
         </>
